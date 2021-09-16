9e330c0c58edd6faa89dc56c19e3f87664a3d381
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-09-16 18:06:30 -0300

    request paralelo

--- async_await/app.js
+++ async_await/app.js
@@ -24,3 +24,20 @@ const listUser = async () => {
 }
 
 listUser(1)
+
+
+
+// --------------------------------------- request paralelo-------------------------------
+
+const getPokemon = async () => {
+  const bulbasaur = fetch('https://pokeapi.co/api/v2/pokemon/1')
+  const charmander = fetch('https://pokeapi.co/api/v2/pokemon/4')
+  const squirtle = fetch('https://pokeapi.co/api/v2/pokemon/7')
+
+
+  const result = await Promise.all([bulbasaur, charmander, squirtle])
+  result.forEach(async response => console.log(await response.json()))
+}
+
+
+getPokemon()
