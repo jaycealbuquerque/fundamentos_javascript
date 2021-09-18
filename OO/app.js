2b3e95f2be9fc9ac25818d924f9bfe5b3211c64b
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-09-18 20:44:09 -0300

    programação orientada a objeto com javascript

---
+++ OO/app.js
@@ -0,0 +1,12 @@
+class User {
+  constructor(name, sobrenome) {
+    this.name = name
+    this.sobrenome = sobrenome
+  }
+}
+
+const name1 = new User('jeyce', 'albuquerque')
+
+
+
+console.log(name1)
