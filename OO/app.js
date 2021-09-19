194ca19481b65ac0025ae454b754b0d5db4f8a85
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-09-18 21:04:56 -0300

    metodos

--- OO/app.js
+++ OO/app.js
@@ -1,12 +1,24 @@
 class User {
+  // propriedades
   constructor(name, sobrenome) {
     this.name = name
     this.sobrenome = sobrenome
+    this.points = 0
+  }
+  // métodos
+  login() {
+    console.log(`${this.name} está logado`)
+    return this
+  }
+  addPoin() {
+    this.points++
+    return `${this.name} tem ${this.points > 1 ? 'pontos' : 'ponto'}`
+
   }
 }
 
 const name1 = new User('jeyce', 'albuquerque')
 
 
-
+name1.login().addPoin()
 console.log(name1)
