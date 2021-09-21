46bfede4a586d9af713b2e93aa53139b635d9e72
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-09-20 22:31:05 -0300

    arrow function

---
+++ arrow functions/app.js
@@ -0,0 +1,19 @@
+const double = (number) => {
+  return nunber * 2
+}
+
+// quando só tem um parametro pode retirar os parentese
+const double = number => {
+  return nunber * 2
+}
+
+
+// quando não tem parametros os parentese são obrigatorio
+const double = () => {
+  return nunber * 2
+}
+
+
+// quando o bloco de codigo tem so uma linha retornando o valor, pode tirar a palavra chave
+// retur e eliminar os parentese.
+const double = () => nunber * 2
